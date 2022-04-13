04f1a613bce79b120d8a7241a06c2684c75317a9
Tasneem <tasneemsherif23@gmil.com>
2022-04-13 21:14:53 +0300

    Initial Commit

---
+++ AnimalKingdom/AnimalKingdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalKingdomApp.swift
+//  AnimalKingdom
+//
+//  Created by Tasneem Toolba on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalKingdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
